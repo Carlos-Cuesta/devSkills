c543f4af3d3794470c598dc3f3559622919a7515
Carlos Cuesta <carlosca@devanddel.com>
2023-10-23 13:55:57 +0200

    Initial commit

---
+++ src/test/java/com/example/devskills/DevSkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSkillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
